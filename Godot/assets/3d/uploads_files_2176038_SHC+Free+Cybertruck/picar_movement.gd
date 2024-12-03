619a823c5c770e39f3e3ff3182e6d6ad08042c65
Fox003 <focloutier@gmail.com>
2024-12-03 12:30:14 -0500

    coop

--- Godot/assets/3d/uploads_files_2176038_SHC+Free+Cybertruck/picar_movement.gd
+++ Godot/assets/3d/uploads_files_2176038_SHC+Free+Cybertruck/picar_movement.gd
@@ -316,7 +316,7 @@ func avoidObstacle():
 			setDesiredSpeed(0.4)
 			setDesiredSteering(0.07)
 			setThonking('HALLELUJAH')
-			await get_tree().create_timer(3.75).timeout #2.75
+			await get_tree().create_timer(4.25).timeout #2.75
 			step = 3
 	elif (step == 3):
 		isStraight = false
